c59af31bb13c47db38f999bf6cd0067dad3eec7e
JKleinne <jkq@jkleinne.com>
2024-10-02 02:30:34 -0400

    feat: add original setup options and plugin to the setup

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,22 @@
 require "nvchad.options"
 
--- add yours here!
+-- Automatically close Neovim if NvimTree is the last open window
+vim.cmd([[
+  autocmd BufEnter * ++nested if winnr('$') == 1 && &filetype == 'NvimTree' | quit | endif
+]])
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- Automatically open NvimTree when Neovim is started
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function(data)
+    -- Check if the buffer is a directory or empty to prevent opening for a specific file
+    local directory = vim.fn.isdirectory(data.file) == 1
+    if directory or data.file == "" then
+      require("nvim-tree.api").tree.open()
+    end
+  end,
+})
+
+-- Set general Neovim options
+vim.opt.number = true        -- Enable line numbers
+vim.opt.expandtab = true     -- Use spaces instead of tabs
+vim.opt.smartindent = true   -- Enable smart indentation
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,6 @@ return {
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,13 +12,12 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    'f-person/git-blame.nvim',
+    event = "VeryLazy",
+    opts = {
+      enabled = true,
+      message_template = "<author> • <date> • [<sha>] <summary>"
+    }
+  }
 }
