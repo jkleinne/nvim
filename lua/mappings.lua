4a1619e993999632257bde6a680ed328f76097bf
JKleinne <jkq@jkleinne.com>
2024-10-04 12:16:42 -0400

    refactor: create default_opt since it's being reused by other mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,20 +1,21 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+local default_opts =  { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Set kubectl.nvim toggle mapping
-vim.keymap.set("n", "<leader>k", '<cmd>lua require("kubectl").toggle()<cr>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>k", '<cmd>lua require("kubectl").toggle()<cr>', default_opts)
 -- Map <leader>gtt to open a terminal and run lazygit
-map("n", "<leader>gtt", ":term lazygit<CR>i", { noremap = true, silent = true })
+map("n", "<leader>gtt", ":term lazygit<CR>i", default_opts)
 
 -- Map <leader>tc to open a terminal and run lazydocker
-map('n', '<leader>dc', ':term lazydocker<CR>i', { noremap = true, silent = true })
+map('n', '<leader>dc', ':term lazydocker<CR>i', default_opts)
 
 -- Map <leader>tb to open an empty tab
-map('n', '<leader>tb', ':enew<CR>', { noremap = true, silent = true })
+map('n', '<leader>tb', ':enew<CR>', default_opts)
 
 -- Map <leader>sq to open a terminal and run lazysql
-map('n', '<leader>sq', ':term lazysql<CR>i', { noremap = true, silent = true })
+map('n', '<leader>sq', ':term lazysql<CR>i', default_opts)
