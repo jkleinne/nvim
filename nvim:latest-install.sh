ec7393d93877c4c0ac7069c93ea8f7ffab69d078
JKleinne <jkq@jkleinne.com>
2024-10-05 00:06:41 -0400

    fix: typo in shebang

--- nvim:latest-install.sh
+++ nvim:latest-install.sh
@@ -1,7 +1,9 @@
-#bin/bash
+#!/bin/bash
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
 
 echo "export PATH=\"$PATH:/opt/nvim-linux64/bin\"" >> ~/.bashrc
+source ~/.bashrc
