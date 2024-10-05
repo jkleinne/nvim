07d8399c7f6713104e501fa9eea7b4ed3efe6f71
JKleinne <jkq@jkleinne.com>
2024-10-05 13:43:41 -0400

    feat: run `posting` when no collection is specified

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,15 +1,15 @@
-require "nvchad.mappings"
+require 'nvchad.mappings'
 
 local map = vim.keymap.set
 local default_opts =  { noremap = true, silent = true }
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map('n', ';', ':', { desc = 'CMD enter command mode' })
+map('i', 'jk', '<ESC>')
 
 -- Set kubectl.nvim toggle mapping
-vim.keymap.set("n", "<leader>k", '<cmd>lua require("kubectl").toggle()<cr>', default_opts)
+map('n', '<leader>k', '<cmd>lua require("kubectl").toggle()<cr>', default_opts)
 -- Map <leader>gtt to open a terminal and run lazygit
-map("n", "<leader>gtt", ":term lazygit<CR>i", default_opts)
+map('n', '<leader>gtt', ':term lazygit<CR>i', default_opts)
 
 -- Map <leader>tc to open a terminal and run lazydocker
 map('n', '<leader>dc', ':term lazydocker<CR>i', default_opts)
@@ -20,11 +20,13 @@ map('n', '<leader>tb', ':enew<CR>', default_opts)
 -- Map <leader>sq to open a terminal and run lazysql
 map('n', '<leader>sq', ':term lazysql<CR>i', default_opts)
 
--- Map <leader>po to run "posting --collection <posting-collection-path>"
-map("n", "<leader>po", function()
-  vim.ui.input({ prompt = "Enter posting collection path: " }, function(input)
-    if input ~= nil and input ~= "" then
-      vim.cmd("term posting --collection " .. input)
+-- Map <leader>po to run 'posting --collection <posting-collection-path>'
+map('n', '<leader>po', function()
+  vim.ui.input({ prompt = 'Enter posting collection path: ' }, function(input)
+    if input ~= nil and input ~= '' then
+      vim.cmd('term posting --collection ' .. input)
+    else
+      vim.cmd('term posting')
     end
   end)
 end, default_opts)
