94c5ce92bbcc83a2720aa3f0834ecc6ae61e683e
JKleinne <jkq@jkleinne.com>
2024-10-14 06:11:10 -0400

    config(catppuccin): set integrations

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -24,11 +24,21 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-      light = "latte",
-      dark = "mocha",
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = {
+        light = "latte",
+        dark = "mocha",
       },
+      integrations = {
+        indent_blankline = {
+          enabled = true,
+          scope_color = 'lavender',
+          colored_indent_levels = false,
+        },
+        neotree = true,
+        window_picker = true,
+        which_key = true
+      }
     })
 
     vim.cmd.colorscheme "catppuccin"
