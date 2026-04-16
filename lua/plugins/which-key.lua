d97c44fc0f712ac5cd52495f0b280667666b4b4f
jkleinne <jkq@jkleinne.com>
2026-04-16 17:57:03 -0400

    feat(mappings): add descriptions, terminal nav, rename leader-x to leader-bd

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,8 +5,13 @@ return {
     local wk = require "which-key"
     wk.setup()
     wk.add {
+      { "<leader>b", group = "Buffer" },
       { "<leader>c", group = "Code" },
+      { "<leader>d", group = "Docker" },
       { "<leader>g", group = "Git" },
+      { "<leader>p", group = "HTTP Client" },
+      { "<leader>r", group = "Refactor" },
+      { "<leader>s", group = "SQL" },
       { "<leader>t", group = "Telescope" },
       { "<leader>w", group = "Workspace" },
       { "<leader>x", group = "Trouble" },
