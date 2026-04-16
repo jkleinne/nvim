6299aa1c5a9b7afa80f2c39e54232bc236b0d054
jkleinne <jkq@jkleinne.com>
2026-04-16 17:41:04 -0400

    feat(lsp): replace lua_ls workspace.library with lazydev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,10 +33,6 @@ return {
           diagnostics = {
             globals = { "vim" },
           },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false,
-          },
           telemetry = {
             enable = false,
           },
