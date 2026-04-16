b28f2019b3cce277c99e94a1ce6a0469ac2b26f7
jkleinne <jkq@jkleinne.com>
2026-04-16 18:54:25 -0400

    refactor(plugins): use lazy.nvim's import shorthand

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,19 +1,4 @@
--- Function to load all Lua files in the 'plugins' directory
-local plugins = {}
-
--- Get a list of all Lua files in the plugins directory
-local plugin_files = vim.fn.globpath(vim.fn.stdpath "config" .. "/lua/plugins", "*.lua", false, true)
-
-for _, file in ipairs(plugin_files) do
-  -- Extract the filename without extension
-  local plugin_name = vim.fn.fnamemodify(file, ":t:r")
-  if plugin_name ~= "init" then -- Skip init.lua to prevent recursion
-    table.insert(plugins, require("plugins." .. plugin_name))
-  end
-end
-
--- Setup lazy.nvim with the collected plugins
-require("lazy").setup(plugins, {
+require("lazy").setup({ { import = "plugins" } }, {
   install = {
     missing = true,
   },
